2573d1b0581fbbf175af91e3c75e8fde770383ea
Ovadya33 <timurgts2710@gmail.com>
2023-09-12 11:00:42 +0300

    calculator

---
+++ view/CalculatorView.js
@@ -0,0 +1,7 @@
+export default class CalculatorView {
+    getHtml(result, isError) {
+        return `<label style ="color:${
+            isError ? 'red' : 'green'
+        };font-size:40px; display:block;text-align:center">${result}</label>`;
+    }
+}
