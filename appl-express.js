8968ea2ac326036c43ba7c5c8fa38a3d5b53d58f
Ovadya33 <timurgts2710@gmail.com>
2023-09-13 17:25:41 +0300

    123

--- appl-express.js
+++ appl-express.js
@@ -1,10 +1,11 @@
-import express, { response } from 'express';
+import express from 'express';
 import { users } from './routes/users.mjs';
 const app = express();
+
 app.use(express.json());
-app.use((reques, response, next) => {
-    reques.body.add = 100;
-    next(reques, response, next);
+app.use((req, res, next) => {
+    req.body.ad = 100;
+    next();
 });
 app.use('/users', users);
 const server = app.listen(8080);
